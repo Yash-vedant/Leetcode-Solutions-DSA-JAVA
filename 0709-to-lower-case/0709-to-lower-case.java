625944f7122be7729573385606e4f86942e4db55
Yash Raj <Yashraj25254@gmail.com>
2026-06-30 14:15:07 +0530

    Time: 0 ms (100%), Space: 42.6 MB (91.87%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String toLowerCase(String s) {
+        
+        char ch [] = s.toCharArray();
+
+        for(int i =0 ; i<ch.length ; i++){
+            if(ch[i]>='A' && ch[i]<='Z'){
+                ch[i] = (char)(ch[i]+32);
+            }
+        }
+
+        String result = new String(ch);
+        return result;
+    }
+}
