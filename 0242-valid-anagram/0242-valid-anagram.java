2e090b46f1bb9df09d40f2a2d40bf1f6c4e3a87c
Yash Raj <Yashraj25254@gmail.com>
2026-07-01 12:34:34 +0530

    Time: 13 ms (28.7%), Space: 46.4 MB (33.66%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        boolean flag = true;
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        Map<Character , Integer> map1 = new HashMap<Character, Integer>();
+		Map<Character , Integer> map2 = new HashMap<Character, Integer>();
+		
+		for(char c : s.toCharArray()) {
+			map1.put(c, map1.getOrDefault(c, 0)+1);
+		}
+		
+		for(char c : t.toCharArray()) {
+			map2.put(c, map2.getOrDefault(c, 0)+1);
+		} 
+
+        return map1.equals(map2);
+    }
+}
